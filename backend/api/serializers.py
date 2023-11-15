a383882e381f97c12b446cee1e3f75b7cb4bc6e1
zvzdt <dlsib@yandex.ru>
2023-11-15 16:15:43 +0700

    base serializers

---
+++ backend/api/serializers.py
@@ -0,0 +1,59 @@
+import base64
+
+from django.contrib.auth import get_user_model
+from django.core.files.base import ContentFile
+from rest_framework import serializers
+#from drf_extra_fields.fields import Base64ImageField
+from serializers import UserCreateSerializer, UserSerializer
+
+from recipes.models import Ingredients, Recipe, Tags
+
+
+User = get_user_model()
+
+
+class CustomUserSerializer(UserSerializer):
+    
+    class Meta:
+        model = User
+        fields  = ('email', 'id', 'username', 
+                   'first_name', 'last_name')
+
+class IngredientsSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Ingredients
+        fields = ('id', 'name', 'measurement_unit')
+        read_only_fields = ('id')
+
+
+class TagsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tags
+        fields = ('id', 'name', 'color', 'slug')
+        read_only_fields = ('id')
+
+
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+
+        return super().to_internal_value(data)
+    
+
+class RecipeSerializer(serializers.ModelSerializer):
+    author = CustomUserSerializer(read_only=True)
+    ingredients = IngredientsSerializer(read_only=True, many=True)
+    tags = TagsSerializer(read_only=True, many=True)
+    image = Base64ImageField(requred=True, allow_null=False)
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'tags', 'author', 'ingredients', 
+                  'name', 'image', 'text', 'cooking_time')
+        read_only_fields = ('id')
