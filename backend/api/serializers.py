a4429f56857cb62c8f6b37bcd53004f4e6953d72
zvzdt <dlsib@yandex.ru>
2023-11-16 13:59:45 +0700

    add base64 field

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,7 +2,7 @@ import base64
 
 from django.core.files.base import ContentFile
 from rest_framework import serializers
-#from drf_extra_fields.fields import Base64ImageField
+from drf_extra_fields.fields import Base64ImageField
 from djoser.serializers import UserSerializer
 
 from recipes.models import Ingredients, Recipe, Tags, User
@@ -29,18 +29,7 @@ class TagsSerializer(serializers.ModelSerializer):
         model = Tags
         fields = ('id', 'name', 'color', 'slug')
         read_only_fields = ('id',)
-
-
-class Base64ImageField(serializers.ImageField):
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-
-        return super().to_internal_value(data)
-    
+ 
 
 class RecipeSerializer(serializers.ModelSerializer):
     author = CustomUserSerializer(read_only=True)
