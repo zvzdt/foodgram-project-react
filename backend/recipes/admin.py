6000a17ecdbc3b3dfec822e4d1f1dc999750d11d
zvzdt <dlsib@yandex.ru>
2023-11-17 16:24:37 +0700

    C:/Program Files/Git/

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Ingredients, Recipe, Tags
+from .models import Ingredients, Recipe, RecipeIngredients, Tags
 
 admin.site.register(Ingredients)
 admin.site.register(Recipe)
+admin.site.register(RecipeIngredients)
 admin.site.register(Tags)
